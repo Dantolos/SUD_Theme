8b68bb0a30ee504e018e033cf328e4042a6f2e47
Dantolos <45291569+Dantolos@users.noreply.github.com>
2023-09-11 11:04:17 +0200

    0.4

---
+++ templates/default.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ *Template Name: Legals Template
+ */
+get_header();
+
+echo '<div class="template-legals-wtapper">';
+echo '<div class="template-legals-container">';
+echo the_content();
+echo '</div>';
+echo '</div>';
+
+get_footer();
