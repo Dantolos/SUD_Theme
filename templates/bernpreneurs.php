5f3b470ddc518a0a640f79e7b941f5c798f061cb
Dantolos <45291569+Dantolos@users.noreply.github.com>
2025-04-15 13:28:55 +0200

    2.76

--- templates/bernpreneurs.php
+++ templates/bernpreneurs.php
@@ -7,27 +7,28 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Favicon Settings -->
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon/site.webmanifest">
+    <link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon/favicon.ico">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-config" content="<?php echo get_template_directory_uri(); ?>/assets/img/favicon/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
 
-     <!-- Favicon Settings -->
-     <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon/apple-touch-icon.png">
-     <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon/favicon-32x32.png">
-     <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon/favicon-16x16.png">
-     <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon/site.webmanifest">
-     <link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon/safari-pinned-tab.svg" color="#5bbad5">
-     <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon/favicon.ico">
-     <meta name="msapplication-TileColor" content="#ffffff">
-     <meta name="msapplication-config" content="<?php echo get_template_directory_uri(); ?>/assets/img/favicon/browserconfig.xml">
-     <meta name="theme-color" content="#ffffff">
+    <meta charset="<?php bloginfo("charset"); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo("name"); ?></title>
 
-     <meta charset="<?php bloginfo("charset"); ?>">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title><?php bloginfo("name"); ?></title>
-
-     <?php wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
+
 <style>
-h1, h2, h3, h4, h5 { color: #1582BE !important; }
-    </style>
+    h1, h2, h3, h4, h5 { color: #1582BE !important; }
+</style>
+
 <body>
 <?php
 echo '<div class="template-default-wrapper">';
--- templates/content-hub.php
+++ templates/content-hub.php
@@ -5,66 +5,67 @@
  */
 get_header();
 
-$args = array(
-     'post_type' => 'podcast', // the post type
-     'posts_per_page' => '-1', 
-     'order' => 'ASC',
-);
-$podcasts = new WP_Query( $args );
+$args = [
+    "post_type" => "podcast", // the post type
+    "posts_per_page" => "-1",
+    "order" => "ASC",
+];
+$podcasts = new WP_Query($args);
 
-$args = array(
-     'post_type' => 'video', // the post type
-     'posts_per_page' => '-1', 
-     'order' => 'ASC',
-);
-$videos = new WP_Query( $args );
+$args = [
+    "post_type" => "video", // the post type
+    "posts_per_page" => "-1",
+    "order" => "ASC",
+];
+$videos = new WP_Query($args);
 
 echo '<div class="template-content-hub-wrapper">';
 
 echo '<div class="template-content-hub-container">';
 echo the_content();
-echo '</div>';
+echo "</div>";
 
 echo '<div class="template-content-hub-section">';
-     echo '<div class="template-content-hub-section-container">';
-          echo '<div class="content-hub-filter-bar">';
-               echo '<div class="template-content-hub-tab">';
-                    echo '<button data-contenttype="video">Videos</button>';
-                    echo '<button data-contenttype="podcast">Podcast</button>'; 
-               echo '</div>';
+echo '<div class="template-content-hub-section-container">';
+echo '<div class="content-hub-filter-bar">';
+echo '<div class="template-content-hub-tab">';
+echo '<button data-contenttype="video">Videos</button>';
+echo '<button data-contenttype="podcast">Podcast</button>';
+echo "</div>";
 
-               echo '<form id="search-form" onsubmit="return handleSearch(event);" >';
-                    echo '<input type="search" name="search" id="search-input" onchange="handleSearch(event)"/>';
-                    echo '<button type="submit" ><img src="'.get_template_directory_uri().'/assets/img/utils/search.svg" /></button>';
-               echo '</form>';
+echo '<form id="search-form" onsubmit="return handleSearch(event);" >';
+echo '<input type="search" name="search" id="search-input" onchange="handleSearch(event)"/>';
+echo '<button type="submit" ><img src="' .
+    get_template_directory_uri() .
+    '/assets/img/utils/search.svg" /></button>';
+echo "</form>";
 
-          echo '</div>';
-          
-          if( is_array($videos->posts ) ) {
-               $videoWidth = '350';
-               $videoHeight = '198';
-               
-               echo '<div class="video-wrapper content-hub-tab-content" data-content="video" data-search="" data-page="1">';
-               echo '<div class="video-container contenthub-container" data-content="video" >';
-                    // Load via JS --> main.js
-               echo '</div>';
-               echo '</div>';
-          }
+echo "</div>";
 
-          if( is_array($podcasts->posts ) ) {
-               echo '<div class="podcasts-wrapper content-hub-tab-content" data-content="podcast">';
-               echo '<div class="podcasts-container contenthub-container">';
-                    // Load via JS --> main.js
-               echo '</div>';
-               echo '</div>';
-          }
+if (is_array($videos->posts)) {
+    $videoWidth = "350";
+    $videoHeight = "198";
 
-     echo '</div>';
-echo '</div>';
-  
+    echo '<div class="video-wrapper content-hub-tab-content" data-content="video" data-search="" data-page="1">';
+    echo '<div class="video-container contenthub-container" data-content="video" >';
+    // Load via JS --> main.js
+    echo "</div>";
+    echo "</div>";
+}
 
-echo '</div>';
+if (is_array($podcasts->posts)) {
+    echo '<div class="podcasts-wrapper content-hub-tab-content" data-content="podcast">';
+    echo '<div class="podcasts-container contenthub-container">';
+    // Load via JS --> main.js
+    echo "</div>";
+    echo "</div>";
+}
 
-echo '</div>';
+echo "</div>";
+echo "</div>";
+
+echo "</div>";
+
+echo "</div>";
 
 get_footer();
